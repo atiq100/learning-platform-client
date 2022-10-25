978caaf939496cf8f392d38d32b0b675b9119632
atiq100 <atiqur1997rj@gmail.com>
2022-10-25 23:35:58 +0600

    footer added

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 import Header from "../Pages/Shared/Header/Header";
 import LeftSideNav from "../Pages/Shared/LeftSideNav/LeftSideNav";
@@ -18,6 +19,7 @@ const Main = () => {
           </div>
         </div>
       </div>
+      <Footer></Footer>
     </div>
   );
 };
