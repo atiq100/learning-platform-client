1c3d02c68388bdaf9bde8e673c0f9038f80cc2de
atiq100 <atiqur1997rj@gmail.com>
2022-10-25 17:30:55 +0600

    setup basic project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#4f46e5",
+        
+"secondary": "#4cd359",
+        
+"accent": "#f472b6",
+        
+"neutral": "#231D2B",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#2dd4bf",
+        
+"success": "#84cc16",
+        
+"warning": "#b91c1c",
+        
+"error": "#E55E57",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
