aae84e33e22e5529bc74bca4b24baabe4f4e34da
atiq100 <atiqur1997rj@gmail.com>
2022-10-27 19:31:43 +0600

    add pdf

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { FaClock, FaVideo } from "react-icons/fa";
+import { FaClock, FaDownload, FaVideo } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
+import Pdf from "react-to-pdf";
 
+const ref = React.createRef()
 const Course = () => {
   const course = useLoaderData();
   const { name, image, price, id, description,Time,duration } = course;
@@ -9,11 +11,16 @@ const Course = () => {
     <div>
         <h2 className="text-xl font-semibold mt-6">Course Details</h2>
         <div className="card w-full md:w-3/4 bg-base-100 shadow-xl mt-4">
+          <div className="flex justify-end mr-4 mt-2 text-primary">
+            <Pdf targetRef={ref} filename="course.pdf">
+            {({ toPdf }) => <button onClick={toPdf} title="download pdf"><FaDownload></FaDownload></button>}
+            </Pdf>
+          </div>
       <figure>
         <img src={image} alt={name} />
       </figure>
       <div className="card-body">
-        <h2 className="card-title">{name}</h2>
+        <h2 ref={ref} className="card-title">{name}</h2>
         <p>{description}</p>
         <p><span className="text-lg font-bold">Price: </span><span className="text-error font-bold">{price}$</span></p>
         <div className="flex justify-items-start">
